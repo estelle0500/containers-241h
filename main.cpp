ffbaa6023a55c61432a2617d6c814a085cb4e6b1
estelle0500 <estelle0500@gmail.com>
2020-12-09 00:53:28 +0800

    Add overlay filesystem

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 #include <fstream>
 #include <string>
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/mount.h>
 
 using std::cout;
+using std::cerr;
 using std::endl;
 using std::string;
 
@@ -24,7 +30,13 @@ int main(int argc, char *argv[]) {
 
     if (command == "create") {
         create_container(argc, argv);
+    } else if (command == "start") {
+        start_container(argc, argv);
+    } else {
+        cout << "Unrecognized command. Command must be one of: create start exec stop" << endl;
     }
+
+    return 0;
 }
 
 void create_container(int argc, char *argv[]) {
@@ -34,19 +46,52 @@ void create_container(int argc, char *argv[]) {
     }
 
     string path = "containers/" + string(argv[2]);
+    string mount_point = path + "/mnt";
+    string image_path = "images/" + string(argv[3]);
     string overlay_upper_path = path + "/upper";
     string working_dir_path = path + "/work";
 
     mkdir(path.c_str(), 0755);
     mkdir(overlay_upper_path.c_str(), 0755);
     mkdir(working_dir_path.c_str(), 0755);
+    mkdir(mount_point.c_str(), 0777);
 
+    // Create a mount point using overlay filesystem
+    string overlay_data = "lowerdir=" + image_path + ",upperdir=" + path + "/upper" 
+                          + ",workdir=" + path + "/work";
+    if (mount(image_path.c_str(), mount_point.c_str(), "overlay", 0, overlay_data.c_str()) != 0) {
+        cerr << "Mount failed: " << strerror(errno) << endl;
+        exit(1);
+    }
+
+    // Create a config file (no particular use right now)
     string config_path = path + "/config.txt";    
     std::ofstream config_file(config_path);
-    string image_path = "images/" + string(argv[3]);
     config_file << image_path;
 }
 
 void start_container(int argc, char *argv[]) {
+    if (argc != 3) {
+        cout << "Usage: ./main start CONTAINER_NAME" << endl;
+        exit(0);
+    }
+    
+    int fork_code = fork();
+    if (fork_code == -1) {
+        cerr << "Could not start container, fork failed" << endl;
+    } else if (fork_code != 0) {
+        cout << "Started container on process " << fork_code << endl;
+        return;
+    }
 
+    // Rest of the function will only be run by the child
+
+    string path = "containers/" + string(argv[2]);
+    string mount_point = path + "/mnt";
+    chroot(mount_point.c_str());
+    chdir("/");
+
+    // TODO make namespace
+    // TODO replace with a shell that will read commands from a pipe
+    execl("/bin/ls", "ls", NULL);
 }
